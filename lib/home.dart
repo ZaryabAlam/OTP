f09ad5c8c4accb92f2a27612c2d76c3f5d93fc87
ZaryabAlam <zaryabalam97@gmail.com>
2023-02-11 21:19:40 +0500

    Project Start

---
+++ lib/home.dart
@@ -0,0 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'login.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String uid = "";
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF0096eb),
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 120,
+                width: 120,
+                decoration: BoxDecoration(
+                    image:
+                        DecorationImage(image: AssetImage("assets/OTP.png"))),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "OTP",
+                style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w100,
+                  fontSize: 35,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Logout",
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 25,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.logout),
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginScreen(),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
