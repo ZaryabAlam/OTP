53cda39802dfaf173e1943111e9b3b7f99b4f897
ZaryabAlam <zaryabalam97@gmail.com>
2023-02-21 21:24:50 +0500

    Project End

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
 import 'home.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
